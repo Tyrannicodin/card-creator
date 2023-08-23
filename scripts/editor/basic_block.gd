7ce8d3425961e61c31bc1a720b45d99f9bb18bfe
Benji42 <63879236+Benji42@users.noreply.github.com>
2023-08-23 15:53:28 -0400

    Change blocks to meshes

--- scripts/editor/basic_block.gd
+++ scripts/editor/basic_block.gd
@@ -1,23 +1,82 @@
-extends TextureRect
+extends Control
 class_name BasicBlock
 
 var placed = false
 var upper_connection = Vector2(35, 5)
+
+var block_template_middle = preload("res://assets/sprites/base_blocks/block_template.tres")
+
+var is_copy = false
+var fields = ["Pick", "INPUT_SPACE"] as Array[String]
+var color = Color.RED
+
+func _ready():
+	if is_copy == false:
+		_generate_block(fields)
 
 func _get_drag_data(at_position):
 	if placed:
 		return
 	var preview = Control.new()
 	var copy = duplicate()
+	copy.is_copy = true
 	preview.add_child(copy)
 	copy.position -= at_position
 	set_drag_preview(preview)
+	var duplicate = duplicate()
+	duplicate.is_copy = true
 	return {
-		"node": duplicate(),
+		"node": duplicate,
 		"name": name,
 		"connections": [Vector2(35, 55)],
 		"offset": at_position,
 	}
+	
+func _generate_block(fields: Array[String]):
+	var mdt = MeshDataTool.new()
+	var m = MeshInstance2D.new()
+	m.mesh = block_template_middle.duplicate()
+	
+	var panel = Panel.new()
+	add_child(panel)
+	panel.add_child(m)
+	mdt.create_from_surface(m.mesh, 0)
+		
+	m.mesh.clear_surfaces()
+
+	var hbox = HBoxContainer.new()
+	panel.add_child(hbox)
+	
+	hbox.set_position(Vector2(0, 20))
+
+	for field in fields:
+		var new_label = Label.new()
+		new_label.text = field
+		new_label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+		hbox.add_child(new_label)
+	await get_tree().process_frame
+	var vertex_count = mdt.get_vertex_count()
+	for vertex_number in mdt.get_vertex_count():
+		var vertex = mdt.get_vertex(vertex_number)
+		mdt.set_vertex(vertex_number, Vector3(vertex.x/10 + 25, vertex.y/10 + 30, vertex.z/10))
+	mdt.commit_to_surface(m.mesh)
+	#Don't know why I need to commit to surface and iterate through the vertices again.
+	#This seems very wasteful but otherwise the geometry becomes weird. Should probably look into this later
+	for vertex_number in mdt.get_vertex_count():
+		var vertex = mdt.get_vertex(vertex_number)
+		if (vertex.x > 40):
+			mdt.set_vertex(vertex_number, Vector3(vertex.x + hbox.size.x - 50, vertex.y, vertex.z))
+	mdt.commit_to_surface(m.mesh)
+	for vertex_number in mdt.get_vertex_count():
+		var vertex = mdt.get_vertex(vertex_number)
+		if upper_connection == null:
+			if vertex.y < 20 && vertex.x > 1 && vertex.x < 30:
+				mdt.set_vertex(vertex_number, Vector3(vertex.x, 5, vertex.z))
+		mdt.set_vertex_color(vertex_number, color)
+	mdt.commit_to_surface(m.mesh)
+	
+	#Set self size for collisions
+	self.custom_minimum_size = Vector2(hbox.size.x,70)
 
 func connect_at(node, connection):
 	if not node.upper_connection:
---
+++ scripts/editor/create_block_background.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var vertices = PackedVector3Array()
+	vertices.push_back(Vector3(0, 1, 0))
+	vertices.push_back(Vector3(1, 0, 0))
+	vertices.push_back(Vector3(0, 0, 0))
+	
+	var arr_mesh = ArrayMesh.new()
+	var arrays = []
+	arrays.resize(Mesh.ARRAY_MAX)
+	arrays[Mesh.ARRAY_VERTEX] = vertices
+	
+	arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
+	var m = MeshInstance3D.new()
+	m.mesh = arr_mesh
+	add_child(m)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
