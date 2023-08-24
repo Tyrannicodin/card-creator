ca685cc9eabed33462e8c60bf036be9a8dc65282
Benji42 <63879236+Benji42@users.noreply.github.com>
2023-08-24 00:29:44 -0400

    Change up some names and add blocks

--- scripts/editor/blocks/header_block.gd
+++ scripts/editor/blocks/header_block.gd
@@ -4,5 +4,3 @@ class_name HeaderBlock
 
 func _init():
 	upper_connection = null
-	color = Color(0.6901960784313725,0.19215686274509805,0.12156862745098039)
-	fields = ["On events", "ect", "ect", "ect", "ect"]
