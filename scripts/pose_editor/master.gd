52a360a361b27df876577d7ffa5fb44a6c4d87a8
Tyrannicodin16 <86689800+Tyrannicodin@users.noreply.github.com>
2023-08-28 23:25:00 +0100

    Make transforms global

--- scripts/pose_editor/master.gd
+++ scripts/pose_editor/master.gd
@@ -8,216 +8,202 @@ var mdt = MeshDataTool.new()
 @onready var items
 
 var steve_texture = preload("res://assets/models/steve.png")
-var alex_texture = preload("res://assets/models/alex.png")
-var selected_item
+var selected_wide
+var selected_slim
+
+# Saving stuff
+var skin:Image
 
 #sliders
-@onready var bend_slider
-@onready var x_slider
-@onready var y_slider
-@onready var z_slider
-@onready var pickup_changes = false
+@onready var bend_slider:Slider
+@onready var x_slider:Slider
+@onready var y_slider:Slider
+@onready var z_slider:Slider
+@onready var pickup_changes := false
 
 enum SkinType {WIDE, SLIM}
-var type_selected = SkinType.WIDE
 
 # Body parts
-var player_mesh
-func _get_player_mesh():
-	player_mesh = {
-		SkinType.WIDE : {
-			"Head" : {
-				"base" : player_wide.get_node("Node/Head/"),
-				"underlay": player_wide.get_node("Node/Head/Head_001"),
-				"overlay": player_wide.get_node("Node/Head/Hat Layer")
-			},
-			"Right Arm" : {
-				"base" : player_wide.get_node("Node/RightArm/"),
-				"underlay": player_wide.get_node("Node/RightArm/Right Arm"),
-				"overlay": player_wide.get_node("Node/RightArm/Right Arm Layer")
-			},
-			"Left Arm" : {
-				"base" : player_wide.get_node("Node/LeftArm/"),
-				"underlay": player_wide.get_node("Node/LeftArm/Left Arm"),
-				"overlay": player_wide.get_node("Node/LeftArm/Left Arm Layer")
-			},
-			"Body" : {
-				"base" : player_wide.get_node("Node"),
-				"underlay": player_wide.get_node("Node/Body/Body_001"),
-				"overlay": player_wide.get_node("Node/Body/Body Layer")
-			},
-			"Right Leg" : {
-				"base" : player_wide.get_node("Node/RightLeg/"),
-				"underlay": player_wide.get_node("Node/RightLeg/Right Leg"),
-				"overlay": player_wide.get_node("Node/RightLeg/Right Leg Layer")
-			},
-			"Left Leg" : {
-				"base" : player_wide.get_node("Node/LeftLeg/"),
-				"underlay": player_wide.get_node("Node/LeftLeg/Left Leg"),
-				"overlay": player_wide.get_node("Node/LeftLeg/Left Leg Layer")
-			}
+@onready var player_mesh = {
+	SkinType.WIDE : {
+		"Head" : {
+			"base" : player_wide.get_node("Node/Head/"),
+			"underlay": player_wide.get_node("Node/Head/Head_001"),
+			"overlay": player_wide.get_node("Node/Head/Hat Layer")
 		},
-		SkinType.SLIM : {
-			"Head" : {
-				"base" : player_slim.get_node("Node/Head/"),
-				"underlay": player_slim.get_node("Node/Head/Head_001"),
-				"overlay": player_slim.get_node("Node/Head/Hat Layer")
-			},
-			"Right Arm" : {
-				"base" : player_slim.get_node("Node/RightArm/"),
-				"underlay": player_slim.get_node("Node/RightArm/Right Arm"),
-				"overlay": player_slim.get_node("Node/RightArm/Right Arm Layer")
-			},
-			"Left Arm" : {
-				"base" : player_slim.get_node("Node/LeftArm/"),
-				"underlay": player_slim.get_node("Node/LeftArm/Left Arm"),
-				"overlay": player_slim.get_node("Node/LeftArm/Left Arm Layer")
-			},
-			"Body" : {
-				"base" : player_slim.get_node("Node"),
-				"underlay": player_slim.get_node("Node/Body/Body_001"),
-				"overlay": player_slim.get_node("Node/Body/Body Layer")
-			},
-			"Right Leg" : {
-				"base" : player_slim.get_node("Node/RightLeg/"),
-				"underlay": player_slim.get_node("Node/RightLeg/Right Leg"),
-				"overlay": player_slim.get_node("Node/RightLeg/Right Leg Layer")
-			},
-			"Left Leg" : {
-				"base" : player_slim.get_node("Node/LeftLeg/"),
-				"underlay": player_slim.get_node("Node/LeftLeg/Left Leg"),
-				"overlay": player_slim.get_node("Node/LeftLeg/Left Leg Layer")
-			}
+		"Right Arm" : {
+			"base" : player_wide.get_node("Node/RightArm/"),
+			"underlay": player_wide.get_node("Node/RightArm/Right Arm"),
+			"overlay": player_wide.get_node("Node/RightArm/Right Arm Layer")
+		},
+		"Left Arm" : {
+			"base" : player_wide.get_node("Node/LeftArm/"),
+			"underlay": player_wide.get_node("Node/LeftArm/Left Arm"),
+			"overlay": player_wide.get_node("Node/LeftArm/Left Arm Layer")
+		},
+		"Body" : {
+			"base" : player_wide.get_node("Node"),
+			"underlay": player_wide.get_node("Node/Body/Body_001"),
+			"overlay": player_wide.get_node("Node/Body/Body Layer")
+		},
+		"Right Leg" : {
+			"base" : player_wide.get_node("Node/RightLeg/"),
+			"underlay": player_wide.get_node("Node/RightLeg/Right Leg"),
+			"overlay": player_wide.get_node("Node/RightLeg/Right Leg Layer")
+		},
+		"Left Leg" : {
+			"base" : player_wide.get_node("Node/LeftLeg/"),
+			"underlay": player_wide.get_node("Node/LeftLeg/Left Leg"),
+			"overlay": player_wide.get_node("Node/LeftLeg/Left Leg Layer")
+		}
+	},
+	SkinType.SLIM : {
+		"Head" : {
+			"base" : player_slim.get_node("Node/Head/"),
+			"underlay": player_slim.get_node("Node/Head/Head_001"),
+			"overlay": player_slim.get_node("Node/Head/Hat Layer")
+		},
+		"Right Arm" : {
+			"base" : player_slim.get_node("Node/RightArm/"),
+			"underlay": player_slim.get_node("Node/RightArm/Right Arm"),
+			"overlay": player_slim.get_node("Node/RightArm/Right Arm Layer")
+		},
+		"Left Arm" : {
+			"base" : player_slim.get_node("Node/LeftArm/"),
+			"underlay": player_slim.get_node("Node/LeftArm/Left Arm"),
+			"overlay": player_slim.get_node("Node/LeftArm/Left Arm Layer")
+		},
+		"Body" : {
+			"base" : player_slim.get_node("Node"),
+			"underlay": player_slim.get_node("Node/Body/Body_001"),
+			"overlay": player_slim.get_node("Node/Body/Body Layer")
+		},
+		"Right Leg" : {
+			"base" : player_slim.get_node("Node/RightLeg/"),
+			"underlay": player_slim.get_node("Node/RightLeg/Right Leg"),
+			"overlay": player_slim.get_node("Node/RightLeg/Right Leg Layer")
+		},
+		"Left Leg" : {
+			"base" : player_slim.get_node("Node/LeftLeg/"),
+			"underlay": player_slim.get_node("Node/LeftLeg/Left Leg"),
+			"overlay": player_slim.get_node("Node/LeftLeg/Left Leg Layer")
 		}
 	}
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_get_player_mesh()
-	
-	type_selected = SkinType.WIDE
 	add_items()
 	
-	_apply_skin(steve_texture, player_mesh[SkinType.WIDE])
-	_apply_skin(alex_texture, player_mesh[SkinType.SLIM])
+	_apply_skin(steve_texture)
 	
 func add_items():
 	if player_mesh == null || items == null:
 		return
-	for key in player_mesh[type_selected]:
+	for key in player_mesh[SkinType.WIDE]:
 		items.add_item(key)
 
-func save(path:String):
-	var saved_player = PackedScene.new()
-	for child in player_wide.get_children():
-		pack_children(player_wide, child)
-	saved_player.pack(player_wide)
-	ResourceSaver.save(saved_player, path)
-
-func load_pose(path:String):
-	var packed_pose_scene:PackedScene = load(path)
-	if not (packed_pose_scene and packed_pose_scene is PackedScene):
-		return
-	var loading_pose = packed_pose_scene.instantiate()
-	if not loading_pose.name in ["player_full", "player_slim"]:
-		return #TODO: More advanced checks on scene we are importing
-	loading_pose.set_meta("filename", path.get_file())
-	_on_skintype_item_selected(0 if loading_pose.name == "player_full" else 1)
-	get_node(loading_pose.name as String).queue_free()
-	await get_tree().process_frame
-	add_child(loading_pose)
-	if loading_pose.name == "player_full":
-		player_wide = loading_pose
-	else:
-		player_slim = loading_pose
-	_get_player_mesh()
-
-func pack_children(root:Node, current_node:Node):
-	current_node.set_owner(root)
-	for child in current_node.get_children():
-		pack_children(root, child)
+#func save(path:String):
+#	var saved_player = PackedScene.new()
+#	for child in player_wide.get_children():
+#		pack_children(player_wide, child)
+#	saved_player.pack(player_wide)
+#	ResourceSaver.save(saved_player, path)
+#
+#func load_pose(path:String):
+#	var packed_pose_scene:PackedScene = load(path)
+#	if not (packed_pose_scene and packed_pose_scene is PackedScene):
+#		return
+#	var loading_pose = packed_pose_scene.instantiate()
+#	if not loading_pose.name in ["player_full", "player_slim"]:
+#		return #TODO: More advanced checks on scene we are importing
+#	loading_pose.set_meta("filename", path.get_file())
+#	_on_skintype_item_selected(0 if loading_pose.name == "player_full" else 1)
+#	get_node(loading_pose.name as String).queue_free()
+#	await get_tree().process_frame
+#	add_child(loading_pose)
+#	if loading_pose.name == "player_full":
+#		player_wide = loading_pose
+#	else:
+#		player_slim = loading_pose
+#
+#func pack_children(root:Node, current_node:Node):
+#	current_node.set_owner(root)
+#	for child in current_node.get_children():
+#		pack_children(root, child)
 
 func _on_x_value_changed(value):
 	if not pickup_changes:
 		return
-	selected_item.base.rotation_degrees.x = value
+	selected_wide.base.rotation_degrees.x = value
+	selected_slim.base.rotation_degrees.x = value
 
 func _on_y_value_changed(value):
 	if not pickup_changes:
 		return
-	selected_item.base.rotation_degrees.y = value
+	selected_wide.base.rotation_degrees.y = value
+	selected_slim.base.rotation_degrees.y = value
 	
 func _on_z_value_changed(value):
 	if not pickup_changes:
 		return
-	selected_item.base.rotation_degrees.z = value
+	selected_wide.base.rotation_degrees.z = value
+	selected_slim.base.rotation_degrees.z = value
 
 func _on_bend_value_changed(value):
 	if not pickup_changes:
 		return
-	selected_item.underlay.bend(value)
-	selected_item.overlay.bend(value)
+	selected_wide.underlay.bend(value)
+	selected_wide.overlay.bend(value)
+	selected_slim.underlay.bend(value)
+	selected_slim.overlay.bend(value)
 
 func _on_items_item_selected(index):
-	_get_player_mesh()
 	pickup_changes = false
 	bend_slider.visible = true
 	
-	selected_item = player_mesh[type_selected][items.get_item_text(index)]
-	selected_item.underlay.set_layer_mask_value(2, true)
-	selected_item.overlay.set_layer_mask_value(2, true)
-	x_slider.value = selected_item.base.rotation_degrees.x
-	y_slider.value = selected_item.base.rotation_degrees.y
-	z_slider.value = selected_item.base.rotation_degrees.z
+	selected_wide = player_mesh[SkinType.WIDE][items.get_item_text(index)]
+	selected_wide.underlay.set_layer_mask_value(2, true)
+	selected_wide.overlay.set_layer_mask_value(2, true)
 	
-	if selected_item.underlay is PlayerLimb:
-		bend_slider.value = selected_item.underlay.get_current_bend()
+	selected_slim = player_mesh[SkinType.SLIM][items.get_item_text(index)]
+	selected_slim.underlay.set_layer_mask_value(2, true)
+	selected_slim.overlay.set_layer_mask_value(2, true)
+	
+	x_slider.value = selected_wide.base.rotation_degrees.x
+	y_slider.value = selected_wide.base.rotation_degrees.y
+	z_slider.value = selected_wide.base.rotation_degrees.z
+	
+	if selected_wide.underlay is PlayerLimb:
+		bend_slider.value = selected_wide.underlay.get_current_bend()
 	else:
 		bend_slider.visible = false
 	pickup_changes = true
 
-func _apply_skin(skin_texture, parts_dict):
-	for key in parts_dict:
-		for segment in parts_dict[key]:
-			var m = parts_dict[key][segment]
-			if m is MeshInstance3D:
-				mdt.create_from_surface(m.mesh, 0)
-				var skin_material = mdt.get_material()
-				skin_material.albedo_texture = skin_texture
-				#skin_material.texture_filter = BaseMaterial3D.TEXTURE_FILTER_NEAREST_WITH_MIPMAPS
-				mdt.set_material(skin_material)
-				m.mesh.clear_surfaces()
-				mdt.commit_to_surface(m.mesh)
+func _apply_skin(skin_texture):
+	for skin in player_mesh.values():
+		for part in skin.values():
+			for segment in part.values():
+				if segment is MeshInstance3D:
+					mdt.create_from_surface(segment.mesh, 0)
+					var skin_material = mdt.get_material()
+					skin_material.albedo_texture = skin_texture
+					#skin_material.texture_filter = BaseMaterial3D.TEXTURE_FILTER_NEAREST_WITH_MIPMAPS
+					mdt.set_material(skin_material)
+					segment.mesh.clear_surfaces()
+					mdt.commit_to_surface(segment.mesh)
 
 func _on_file_dialog_file_selected(path):
 	var texture = ImageTexture.new()
 	var image = Image.new()
 	image.load(path)
 	texture.set_image(image)
-	_apply_skin(texture, player_mesh[type_selected])
-
-func _carry_info(from_mesh: SkinType, to_mesh: SkinType):
-	var parts_dict = player_mesh[from_mesh]
-	for key in parts_dict:
-		if parts_dict[key] == selected_item:
-			selected_item = player_mesh[to_mesh][key]
-		for segment in parts_dict[key]:
-			var from_segment = player_mesh[from_mesh][key][segment]
-			var to_segment = player_mesh[to_mesh][key][segment]
-			to_segment.rotation_degrees.x = from_segment.rotation_degrees.x
-			to_segment.rotation_degrees.y = from_segment.rotation_degrees.y
-			to_segment.rotation_degrees.z = from_segment.rotation_degrees.z
-			if to_segment is PlayerLimb: 
-				to_segment.bend(from_segment.get_current_bend())
+	_apply_skin(texture)
 
 func _on_skintype_item_selected(index):
 	if index == 0:
 		player_slim.visible = false
 		player_wide.visible = true
-		type_selected = SkinType.WIDE
-		_carry_info(SkinType.SLIM, SkinType.WIDE)
 	if index == 1:
 		player_wide.visible = false
 		player_slim.visible = true
-		type_selected = SkinType.SLIM
-		_carry_info(SkinType.WIDE, SkinType.SLIM)
