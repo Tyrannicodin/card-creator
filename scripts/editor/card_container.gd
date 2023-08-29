cce0d21eebd96c5501ae877eb93046ef698880cc
Tyrannicodin16 <86689800+Tyrannicodin@users.noreply.github.com>
2023-08-29 23:21:51 +0100

    Renaming to prepare for property manager

--- scripts/editor/card_container.gd
+++ scripts/editor/card_container.gd
@@ -1,7 +1,7 @@
 extends Panel
 
 @onready var card = $card
-@onready var types = $"../PropertyContainer/MarginContainer2/VBoxContainer/GridContainer/Type"
+@onready var types = $"../MarginContainer/VBoxContainer/GridContainer/Type"
 
 @onready var preview_viewport = $"../../../pose_editor".get_node("PreviewViewport")
 
